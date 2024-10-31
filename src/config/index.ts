9d550bc8392c34b459cfb517e22b1501fe1f430d
aerials <nosvengeance@gmail.com>
2024-10-31 23:02:26 +0700

    feat: add config file

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+interface ConfigProps {
+  encKey: CryptoJS.lib.WordArray;
+  encIV: CryptoJS.lib.WordArray;
+}
+
+const CONFIG: ConfigProps = {
+  encKey: CryptoJS.enc.Hex.parse(process.env.NEXT_PUBLIC_APP_ENC_KEY || "x"),
+  encIV: CryptoJS.enc.Hex.parse(process.env.NEXT_PUBLIC_APP_ENV_IV || "x")
+};
+
+export default CONFIG;
