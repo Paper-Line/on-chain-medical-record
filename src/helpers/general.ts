8960c49b4a376d9fa45adce2fab7cc20a8f13a88
aerials <nosvengeance@gmail.com>
2024-10-31 23:03:13 +0700

    feat: created General helper

---
+++ src/helpers/general.ts
@@ -0,0 +1,36 @@
+import CryptoJS from "crypto-js";
+
+import CONFIG from "@/config";
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export const encrypt = (text: string) => {
+  try {
+    const encrypted = CryptoJS.AES.encrypt(text, CONFIG.encKey, {
+      iv: CONFIG.encIV
+    }).toString();
+    const encoded = CryptoJS.enc.Base64.parse(encrypted).toString(CryptoJS.enc.Hex);
+    return encoded;
+  } catch (e: unknown) {
+    return null;
+  }
+};
+
+export function decrypt(encrypted: string) {
+  try {
+    const decoded = CryptoJS.enc.Hex.parse(encrypted).toString(CryptoJS.enc.Base64);
+    const decrypted = CryptoJS.AES.decrypt(decoded, CONFIG.encKey, {
+      iv: CONFIG.encIV
+    });
+    return decrypted.toString(CryptoJS.enc.Utf8);
+  } catch (e: unknown) {
+    return null;
+  }
+}
+
+export function parser(string: string) {
+  try {
+    return JSON.parse(string);
+  } catch (e: unknown) {
+    return null;
+  }
+}
