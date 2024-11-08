75fdf679885e0f18770bd760b8fdeea79ded0509
aerials <nosvengeance@gmail.com>
2024-11-08 21:28:38 +0700

    feat: created Button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,17 +1,20 @@
-import type {ReactNode} from "react";
+import type { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface ButtonProps {
   children: ReactNode;
   onClick: (() => Promise<void>) | (() => void);
+  className?: string;
   disabled?: boolean;
 }
 
-export const Button = ({children, onClick, disabled = false}: ButtonProps) => {
+export const Button = ({ children, onClick, className, disabled = false }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`flex items-center gap-2 border-black dark:border-lavender-blue-500 border-[3px] transition-all rounded-sm py-1 px-8 my-2 font-semibold text-white bg-lavender-blue-500 dark:bg-black shadow-[5px_5px_0px_rgba(0,0,0,1)] dark:shadow-[5px_5px_0px_#7888ff] ${disabled ? "opacity-25" : "hover:bg-lavender-blue-600 dark:hover:bg-lavender-blue-300 dark:hover:text-black active:bg-lavender-blue-400 dark:active:bg-lavender-blue-500 active:shadow-none active:translate-x-[5px] active:translate-y-[5px]"}`}>
+      className={twMerge("mt-1 w-full py-2 font-semibold bg-green-50 text-green-500 border border-green-400 rounded-xl", className)}
+    >
       {children}
     </button>
   );
