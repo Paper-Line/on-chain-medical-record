83d6cf4616e31b3311aea34f635861e755f681ec
aerials <nosvengeance@gmail.com>
2024-11-09 18:59:35 +0700

    feat: Button loading

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -6,19 +6,20 @@ interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes
   onClick: (() => Promise<void>) | (() => void);
   className?: string;
   disabled?: boolean;
+  loading?: boolean;
 }
 
 export const Button = (props: ButtonProps) => {
-  const { children, onClick, className, disabled, ...restOfProps } = props;
+  const { children, onClick, className, disabled, loading, ...restOfProps } = props;
 
   return (
     <button
       onClick={onClick}
-      disabled={disabled}
+      disabled={disabled || loading}
       className={twMerge("mt-1 w-full py-2 font-semibold bg-green-50 text-green-500 border border-green-400 rounded-xl", className)}
       {...restOfProps}
     >
-      {children}
+      {loading ? "Loading..." : children}
     </button>
   );
 };
