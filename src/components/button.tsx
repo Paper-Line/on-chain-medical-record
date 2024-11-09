d8d1f4d44423b734ffea5b1ec1de0c125e2d1d3d
aerials <nosvengeance@gmail.com>
2024-11-09 18:48:42 +0700

    refactor: Button props management

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,22 @@
 import type { ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface ButtonProps {
+interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   children: ReactNode;
   onClick: (() => Promise<void>) | (() => void);
   className?: string;
   disabled?: boolean;
 }
 
-export const Button = ({ children, onClick, className, disabled = false }: ButtonProps) => {
+export const Button = (props: ButtonProps) => {
+  const { children, onClick, className, disabled, ...restOfProps } = props;
+
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       className={twMerge("mt-1 w-full py-2 font-semibold bg-green-50 text-green-500 border border-green-400 rounded-xl", className)}
+      {...restOfProps}
     >
       {children}
     </button>
