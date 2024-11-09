602f5d8b6868d285e4985c54a98bbbed89c417fb
aerials <nosvengeance@gmail.com>
2024-11-09 22:16:59 +0700

    feat: created bigIntStampToUniversalStamp

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -41,10 +41,14 @@ export function bigIntToTimestamp(milliseconds: bigint): number {
   return millisecondsNumber;
 }
 
-export function convertMillisecondsToYYYYMMDDHHMM(milliseconds: bigint) {
-  const ms = bigIntToTimestamp(milliseconds);
+export function bigIntStampToUniversalStamp(milliseconds: bigint) {
+  const bigIntTs = bigIntToTimestamp(milliseconds);
 
-  const date = new Date(ms / 1000000);
+  return bigIntTs / 1_000_000;
+}
+
+export function convertMillisecondsToYYYYMMDDHHMM(milliseconds: number) {
+  const date = new Date(milliseconds);
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
