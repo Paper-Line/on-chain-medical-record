015c9258af89191355855474bd47b584d3027480
mars-area <ilhammargatama@gmail.com>
2024-11-09 22:57:36 +0700

    style: clean landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { Hero, Navbar } from "@/ui/landing";
-import { Footer } from "@/components/footer";
-import { Login } from "@/components/login";
 
 import config from "@/config";
 import useAuth from "@/hooks/useAuth";
@@ -19,12 +17,6 @@ export default function Home() {
       <div className="relative isolate min-h-[100dvh]">
         <Navbar />
         <Hero />
-        <main className="mx-auto max-w-screen-2xl py-16 px-8 md:px-24">
-          <h1 className="text-5xl md:text-6xl font-bold tracking-tight md:pt-24">Medical Tracker</h1>
-          <Login />
-        </main>
-
-        <Footer />
       </div>
     </>
   );
