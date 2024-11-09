51c541c85510eb2bf76258b099575ca3c0e6f50b
aerials <nosvengeance@gmail.com>
2024-11-09 21:53:38 +0700

    feat: created normal timestamp date formatter

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -40,3 +40,16 @@ export function bigIntToTimestamp(milliseconds: bigint): number {
 
   return millisecondsNumber;
 }
+
+export function convertMillisecondsToYYYYMMDDHHMM(milliseconds: bigint) {
+  const ms = bigIntToTimestamp(milliseconds);
+
+  const date = new Date(ms / 1000000);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
